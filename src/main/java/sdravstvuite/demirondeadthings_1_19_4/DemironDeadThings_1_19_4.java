d03f919b3f475a087ddb5e42694e3a89618c9468
BuildTools <unconfigured@null.spigotmc.org>
2023-06-11 01:07:29 +0500

    Initial commit

---
+++ src/main/java/sdravstvuite/demirondeadthings_1_19_4/DemironDeadThings_1_19_4.java
@@ -0,0 +1,17 @@
+package sdravstvuite.demirondeadthings_1_19_4;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DemironDeadThings_1_19_4 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
